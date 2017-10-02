8bd65d79ae611a84f1ec73efe149f700f7e3339f
dbulavinovGit <git.work.bda@gmail.com>
2017-10-02 16:13:14 +0300

    second iteration

---
+++ js/main.js
@@ -0,0 +1,111 @@
+jQuery(function() {
+	initTab();
+});
+
+function initTab() {
+	jQuery('.tab-hodler').tab({
+		opener: 'input[type=checkbox]'
+	});
+	jQuery('.tab-hodler').tab({
+		opener: 'input[type=radio]'
+	});
+	jQuery('.tab-hodler').tab({
+		opener: '.opener',
+		dataAttr: 'href'
+	});
+	jQuery('.tab-hodler').tab({
+		opener: 'select',
+		dataAttr: 'data-tab'
+	});
+}
+
+;(function($){
+	function Tab(options) {
+		this.options = $.extend({
+			activeClass: 'active',
+			opener: 'input[type=checkbox]',
+			slide: '.tab',
+			dataAttr: 'data-target',
+			openDefault: true,
+		}, options);
+		this.init();
+	}
+
+	Tab.prototype = {
+		init: function() {
+			if (this.options.holder) {
+				this.findElements();
+				this.attachEvents();
+				this.opendDefaultTab(this.activeTab);
+			}
+		},
+		findElements: function() {
+			this.holder = $(this.options.holder);
+			this.opener = this.holder.find(this.options.opener);
+			this.tabs = this.holder.find(this.options.slide);
+			this.activeLink = this.opener.filter('.' + this.options.activeClass);
+			this.activeTab = $(this.activeLink.attr(this.options.dataAttr));
+		},
+		attachEvents: function() {
+			var self = this;
+
+			this.eventHandler = function(e) {
+				e.preventDefault();
+				var target = $(e.target);
+				var slide;
+
+				if (self.isOpenerSelect()) {
+					slide = $(target.val());
+				} else {
+					slide = $(target.attr(self.options.dataAttr))
+				}
+
+				self.toggleTab(slide, target);
+			}
+
+			if (this.isOpenerSelect()) {
+				this.opendDefaultTab($(this.opener.attr(this.options.dataAttr)));
+
+				this.opener.on('change', this.eventHandler);
+			} else {
+				this.opener.on('click', this.eventHandler);
+			}
+		},
+		opendDefaultTab: function(activeTab) {
+			if (this.options.openDefault) {
+				activeTab.css({
+					display: 'block'
+				});
+			}
+		},
+		isOpenerSelect: function() {
+			return this.options.opener == 'select'
+		},
+		toggleTab: function(slide, opener) {
+			this.opener.removeClass(this.options.activeClass);
+			this.tabs.css({
+				display: 'none'
+			});
+
+			slide.css({
+				display: 'block'
+			});
+
+			opener.addClass(this.options.activeClass);
+		},
+		destroy: function() {
+			if(this.isOpenerSelect()) {
+				this.opener.off('click', this.eventHandler)
+			} else {
+				this.opener.off('change', this.eventHandler);
+			}
+		}
+	};
+
+	// jQuery plugin interface
+	$.fn.tab = function(opt) {
+		return this.each(function() {
+			jQuery(this).data('Tab', new Tab($.extend(opt, { holder: this })));
+		});
+	};
+}(jQuery));
