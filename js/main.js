222f3da2e154408116f44d745b22172bb4fad30f
dbulavinovGit <git.work.bda@gmail.com>
2017-10-18 11:51:26 +0300

    fixes

--- js/main.js
+++ js/main.js
@@ -3,43 +3,33 @@ jQuery(function() {
 });
 
 function initTab() {
-	jQuery('.tab-hodler').tab({
-		opener: 'input[type=checkbox]',
-		hideOnSecondClick: true
-	});
-	jQuery('.tab-hodler').tab({
-		opener: 'input[type=radio]'
-	});
+	var select = jQuery("select");
+
 	jQuery('.tab-hodler').tab({
 		opener: '.opener',
-		openDefault: false,
-		hideOnSecondClick: true,
-		dataAttr: 'href'
-	});
-	jQuery('.select-tab-hodler').tab({
-		opener: 'select',
-		dataAttr: 'data-tab',
-		event: 'change',
 		openDefault: true,
+		classToParent: true,
+		dataAttr: 'href',
 
 		onInit: function(self) {
-			var opener = jQuery(self.options.opener);
-			var slide;
-
-			function clickHandler() {
-				slide = jQuery(opener.val());
-
-				self.showTab(slide, opener)
-			}
-
-			if (self.options.openDefault) {
-				slide = jQuery(opener.attr(self.options.dataAttr));
-
-				self.showTab(slide, opener)
-			}
-
-			opener.on(self.options.event, clickHandler);
+			select.on('change', function() {
+				var item = jQuery(this)
+				var activeIndex = item.children('option:selected').index();
+				self.getActiveItems(activeIndex);
+				self.toggleTab();
+			});
+		},
+		onShow: function(self) {
+			select.children('option');
+			select.children().eq(self.activeIndex).attr('selected', 'selected')
 		}
+	});
+	jQuery('.tab-hodler2').tab({
+		opener: 'input[type=checkbox]',
+		hideOnSecondClick: true,
+	});
+	jQuery('.tab-hodler3').tab({
+		opener: 'input[type=radio]'
 	});
 }
 
@@ -47,12 +37,13 @@ function initTab() {
 	function Tab(options) {
 		this.options = $.extend({
 			activeClass: 'active',
+			classToParent: false,
 			opener: 'input[type=checkbox]',
 			slide: '.tab',
 			dataAttr: 'data-target',
 			event: 'click',
-			openDefault: true,
 			hideOnSecondClick: false,
+			openDefault: true,
 		}, options);
 		this.init();
 	}
@@ -62,62 +53,70 @@ function initTab() {
 			if (this.options.holder) {
 				this.findElements();
 				this.attachEvents();
-				this.opendDefaultTab(this.activeTab);
+				this.opendDefaultTab();
 				this.makeCallback('onInit', this);
 			}
 		},
 		findElements: function() {
 			this.holder = $(this.options.holder);
-			this.opener = this.holder.find(this.options.opener);
+			this.openers = this.holder.find(this.options.opener);
 			this.tabs = this.holder.find(this.options.slide);
-			this.activeLink = this.opener.filter('.' + this.options.activeClass);
-			this.activeTab = $(this.activeLink.attr(this.options.dataAttr));
 		},
 		attachEvents: function() {
 			var self = this;
 
 			this.eventHandler = function(e) {
 				e.preventDefault();
-				var target = $(e.target);
-				var slide;
-
-				slide = $(target.attr(self.options.dataAttr))
-				self.toggleTab(slide, target);
+				self.currentOpener = e.target;
+				self.activeIndex = self.getClassTarget(self.openers).index(self.getClassTarget($(self.currentOpener)));
+				self.getActiveItems(self.activeIndex);
+				self.toggleTab();
 			}
 
-			this.opener.on(this.options.event, this.eventHandler);
+			this.openers.on(this.options.event, this.eventHandler);
 		},
-		opendDefaultTab: function(activeTab) {
+		opendDefaultTab: function() {
 			if (this.options.openDefault) {
-				activeTab.addClass(this.options.activeClass);
+				this.activeIndex = this.getClassTarget(this.openers).index(this.getClassTarget(this.openers).filter('.' + this.options.activeClass));
+				this.getActiveItems(this.activeIndex);
+				this.showTab();
 			}
 		},
-		isOpened: function(opener) {
-			return opener.hasClass(this.options.activeClass);
+		isOpened: function() {
+			return this.activeLink.hasClass(this.options.activeClass);
 		},
-		showTab: function(activeTab, opener) {
-			this.hideTab(activeTab, opener);
-
-			activeTab.addClass(this.options.activeClass);
-			opener.addClass(this.options.activeClass);
-			this.makeCallback('onShow', activeTab, opener);
+		getClassTarget: function(item) {
+			return this.options.classToParent ? item.parent() : item;
 		},
-		hideTab: function(activeTab, opener) {
-			this.opener.removeClass(this.options.activeClass);
+		getActiveItems: function(index) {
+			this.activeTab = $(this.openers.eq(index).attr(this.options.dataAttr));
+			this.activeLink = this.getClassTarget(this.openers.eq(index));
+		},
+		showTab: function() {
+			this.activeTab.addClass(this.options.activeClass);
+			this.activeLink.addClass(this.options.activeClass);
+			this.makeCallback('onShow', this);
+		},
+		hideTab: function() {
+			this.getClassTarget(this.openers).removeClass(this.options.activeClass);
 			this.tabs.removeClass(this.options.activeClass);
-			this.makeCallback('onHide', activeTab, opener);
+			this.makeCallback('onHide', this);
 		},
-		toggleTab: function(activeTab, opener) {
-			if(!this.isOpened(opener)) {
-				this.showTab(activeTab, opener);
-			} else {
-				if(this.options.hideOnSecondClick) {
-					this.hideTab(activeTab, opener);
+		toggleTab: function() {
+			if(this.options.hideOnSecondClick) {
+				if(this.isOpened()) {
+					this.hideTab();
+				} else {
+					this.hideTab();
+					this.showTab();
 				}
+			} else {
+				this.hideTab();
+				this.showTab();
 			}
 		},
 		destroy: function() {
-			this.opener.off(this.options.event, this.eventHandler);
+			this.openers.off(this.options.event, this.eventHandler);
 			this.tabs.removeClass(this.options.activeClass);
 		},
 		makeCallback: function(name) {
@@ -131,8 +130,23 @@ function initTab() {
 
 	// jQuery plugin interface
 	$.fn.tab = function(opt) {
+		var args = Array.prototype.slice.call(arguments);
+		var method = args[0];
+
 		return this.each(function() {
-			jQuery(this).data('Tab', new Tab($.extend(opt, { holder: this })));
+			var $holder = jQuery(this);
+			var instance = $holder.data('Tab');
+
+			if (typeof opt === 'object' || typeof opt === 'undefined') {
+				$holder.data('Tab', new Tab($.extend({
+					holder: this
+				}, opt)));
+			} else if (typeof method === 'string' && instance) {
+				if (typeof instance[method] === 'function') {
+					args.shift();
+					instance[method].apply(instance, args);
+				}
+			}
 		});
 	};
 }(jQuery));
